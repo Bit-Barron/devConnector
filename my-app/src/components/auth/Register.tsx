5880069e459b5bb7e33710ee1e8cf5875dce7abe
DoniiBoyJr <doniiboyjrr@gmail.com>
2022-09-05 16:09:51 +0200

    feat: server

--- my-app/src/components/auth/Register.tsx
+++ my-app/src/components/auth/Register.tsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
+import { url } from "../../utils/constants";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "don",
+    email: "don@don",
+    password: "don",
+    password2: "don",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+
+        const res = await axios.post(`${url}/api/users`, newUser);
+        console.log(res.data);
+      } catch (err: any) {
+        console.error(err);
+      }
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </>
+  );
+};
 
 export default Register;
