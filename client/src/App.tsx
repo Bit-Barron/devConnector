270f6d1d7417e5e0968025caecdc9e1a2e0b32f0
DoniiBoyJr <doniiboyjrr@gmail.com>
2022-09-06 18:02:05 +0200

    feat: redux1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,22 +5,27 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//redux
+import { Provider } from "react-redux";
+import store from "redux";
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
 
-        <section className="container">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </section>
-      </>
-    </Router>
+          <section className="container">
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </section>
+        </>
+      </Router>
+    </Provider>
   );
 };
 
