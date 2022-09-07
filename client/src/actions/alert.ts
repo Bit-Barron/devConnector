7ed48a797a1e289c5b6cdf0d1f9dea5729ac3bc3
DoniiBoyJr <doniiboyjrr@gmail.com>
2022-09-07 12:40:20 +0200

    feat: alert

--- client/src/actions/alert.ts
+++ client/src/actions/alert.ts
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid';
 import { REMOVE_ALERT, SET_ALERT } from './types';
 
 export const setAlert =
-  (msg: any, alertType: any) => (dispatch: (arg0: { type: any }) => void) => {
+  (msg: any, alertType: any, timeout = 5000) => (dispatch: (arg0: { type: any }) => void) => {
     const id = uuid();
     dispatch({
       type: SET_ALERT,
@@ -11,5 +11,5 @@ export const setAlert =
     });
 
     //@ts-ignore
-    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
   };
